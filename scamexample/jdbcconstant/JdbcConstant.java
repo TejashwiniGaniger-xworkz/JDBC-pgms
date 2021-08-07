63467516ac9f2c5ea892331a5d045112bdbce821
TejashwiniGaniger-xworkz <85569258+TejashwiniGaniger-xworkz@users.noreply.github.com>
2021-08-07 21:31:55 +0530

    JDBC Constants

--- scamexample/jdbcconstant/JdbcConstant.java
+++ scamexample/jdbcconstant/JdbcConstant.java
@@ -3,8 +3,8 @@ package com.xworkz.jdbcconstant;
 public class JdbcConstant {
 	
 	public static final String username = "root";
-	public static final String password = "sahana@123";
-	public static final String url = "jdbc:mysql://localhost:3306/apriltransformers";
+	public static final String password = "Tejashwini@123";
+	public static final String url = "jdbc:mysql://localhost:3306/jdbc";
 	public static final String fqnOfDriverImpl = "com.mysql.cj.jdbc.Driver";
 
 }
